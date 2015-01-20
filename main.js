80bf29d5f35004e5a063a34b0914c1ea92c0e039
Alon Dahari <climbinghobo@gmail.com>
2015-01-20 15:14:42 -0700

    fix second lightbox bug

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).on('ready', function() {
   var $lightbox = $('<div class="center lightbox">').html('<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>');
   var $dismissButton = $('<button class="dismiss">Close</button>');
+  $lightbox.append($dismissButton);
 
   $('.button').on('click', function(){
     $('body').append('Click!');
@@ -34,10 +35,10 @@ $(document).on('ready', function() {
   });
 
   $('.big-button').on('click', function(){
-    $('body').append($lightbox.append($dismissButton));
+    $('body').append($lightbox);
   });
 
   $dismissButton.on('click', function(){
-    $lightbox.remove();
+    $lightbox.detach();
   });
 });
